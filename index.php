02c3958b79a3481a88fc7c10c2971a4b95840821
Denis Popov <akadan47@gmail.com>
2014-01-15 05:43:01 +0400

    add index page

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Template engine benchmark</title>
+</head>
+<body>
+    <header>
+        <h1>Template engine benchmark</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="/gekkon/" target="_blank">Gekkon</a> (<a href="/gekkon/?include" target="_blank">with includes</a>)</li>
+            <li><a href="/smarty/" target="_blank">Smarty</a> (<a href="/smarty/?include" target="_blank">with includes</a>)</li>
+            <li><a href="/twig/" target="_blank">Twig</a> (<a href="/twig/?include" target="_blank">with includes</a>)</li>
+        </ul>
+    </nav>
+</body>
+</html>
