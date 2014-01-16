98dd7a9361e8f49c211ce80147081c827e4a32f5
Denis Popov <akadan47@gmail.com>
2014-01-16 11:37:31 +0400

    Live benchmark

---
+++ _files/js/benchmark.js
@@ -0,0 +1,71 @@
+function Benchmark(engines, options) {
+    this.options = {
+        "requests": 1000,
+        "include": true,
+        "on_start": function(){},
+        "on_step": function(){},
+        "on_end": function(){},
+        "on_complete": function(){}
+    };
+    $.extend(this.options, options);
+
+    this.engines = engines.slice(0);
+    this.request_count = this.options.requests;
+    this.data = [];
+    this.is_first = true;
+
+    var self = this;
+
+    function average(arr) {
+        var x, sum = 0;
+        for (x = 0; x < arr.length; x++) {
+            arr[x] = +arr[x];
+            sum += arr[x];
+        }
+        return (sum / arr.length);
+    }
+
+    function bench() {
+        if (self.request_count > 0) {
+            $.ajax({
+                type: 'GET',
+                data: {'json':true},
+                url: self.engines[0].url,
+                dataType: 'json',
+                success: function(json) {
+                    var time = json['time'];
+                    if (!self.is_first) {
+                        self.data.push(time);
+                        self.request_count -= 1;
+                        self.options.on_step(self.engines[0], (time).toFixed(8), self.request_count);
+                    } else {
+                        self.is_first = false;
+                        self.options.on_start(self.engines[0]);
+                    }
+                    bench();
+                },
+                error: bench
+            });
+        } else {
+            var average_time = average(self.data) * 1000;
+            var min_time = Math.min.apply({}, self.data) * 1000;
+            var max_time = Math.max.apply({}, self.data) * 1000;
+            self.options.on_end(self.engines[0], average_time.toFixed(2), min_time.toFixed(2), max_time.toFixed(2));
+
+            self.engines.shift();
+            self.data = [];
+            self.is_first = true;
+            if (self.engines.length) {
+                self.request_count = self.options.requests;
+                bench();
+            } else {
+                self.options.on_complete();
+            }
+        }
+    }
+
+    this.start = function () {
+        bench();
+    };
+
+}
--- index.php
+++ index.php
@@ -1,19 +1,153 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Template engine benchmark</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Typical page benchmark for PHP template engines</title>
+    <link rel="stylesheet" href="/_files/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/_files/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="/_files/css/style.css">
+    <script type="text/javascript" src="/_files/js/jquery-1.10.2.min.js"></script>
+    <script type="text/javascript" src="/_files/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/_files/js/smoothie.js"></script>
+    <script type="text/javascript" src="/_files/js/jquery.tablesorter.min.js"></script>
+    <script type="text/javascript" src="/_files/js/benchmark.js"></script>
+    <script type="text/javascript">
+            var ENGINES = [
+                {
+                    "id": "gekkon",
+                    "name": "Gekkon",
+                    "url": "/gekkon/"
+                },
+                {
+                    "id": "smarty",
+                    "name": "Smarty",
+                    "url": "/smarty/"
+                },
+                {
+                    "id": "twig",
+                    "name": "Twig",
+                    "url": "/twig/"
+                },
+                {
+                    "id": "gekkon_includes",
+                    "name": "Gekkon with includes",
+                    "url": "/gekkon/?include"
+                },
+                {
+                    "id": "smarty_includes",
+                    "name": "Smarty with includes",
+                    "url": "/smarty/?include"
+                },
+
+                {
+                    "id": "twig_includes",
+                    "name": "Twig with includes",
+                    "url": "/twig/?include"
+                }
+            ];
+    </script>
 </head>
+
 <body>
-    <header>
-        <h1>Template engine benchmark</h1>
-    </header>
-    <nav>
-        <ul>
-            <li><a href="/gekkon/" target="_blank">Gekkon</a> (<a href="/gekkon/?include" target="_blank">with includes</a>)</li>
-            <li><a href="/smarty/" target="_blank">Smarty</a> (<a href="/smarty/?include" target="_blank">with includes</a>)</li>
-            <li><a href="/twig/" target="_blank">Twig</a> (<a href="/twig/?include" target="_blank">with includes</a>)</li>
-        </ul>
-    </nav>
+
+<div class="container bs-docs-container">
+
+    <div class="jumbotron">
+        <button class="btn btn-lg btn-success" id="start">Start benchmark!</button>
+    </div>
+
+    <div class="row">
+
+        <div class="col-md-7">
+            <div id="process"></div>
+        </div>
+
+        <div class="col-md-5">
+            <div class="bs-sidebar hidden-print affix-top">
+                <div class="panel panel-default hidden" id="results">
+                    <div class="panel-heading"><h3 class="panel-title">Results (ms)</h3></div>
+
+                    <div class="panel-body">
+                    <table class="table" id="tablesort">
+                        <thead>
+                            <tr>
+                                <th style="width: 90%;">Name</th>
+                                <th>Avg</th>
+                                <th>Min</th>
+                                <th>Max</th>
+                            </tr>
+                        </thead>
+                        <tbody id="score">
+
+                        </tbody>
+                    </table>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+
+<script type="text/javascript">
+    $(function () {
+
+        function start(engine){
+            engine.line = new TimeSeries();
+            engine.chart = new SmoothieChart({millisPerPixel:53,grid:{fillStyle:'#ffffff',strokeStyle:'rgba(119,119,119,0.11)',sharpLines:true,borderVisible:false},labels:{fillStyle:'#242424'}});
+            engine.chart.addTimeSeries(engine.line, {lineWidth:0.5, strokeStyle:'#03dc00'});
+            engine.chart.streamTo(document.getElementById("chart_"+engine.id), 100);
+            engine.chart.start();
+            $('#panel_'+engine.id).addClass('panel-warning');
+        }
+
+        function step(engine, time, count) {
+            engine.line.append(new Date().getTime(), time);
+        }
+
+        function end(engine, avg, min, max) {
+            engine.chart.stop();
+            $('.panel-warning').removeClass('panel-warning');
+            $('#score').append($('<tr><td>'+engine.name+'</td><td>'+avg+'</td><td>'+min+'</td><td>'+max+'</td></tr>'));
+        }
+
+        function complete() {
+            $("#tablesort").trigger('update').tablesorter();
+            $('#results').addClass('panel-warning');
+            $('#start').text('Restart Benchmark!').removeAttr('disabled');
+        }
+
+        $('#start').on('click', function(){
+            $(this).attr('disabled', 'disabled');
+            $('#process').children().remove();
+            $('#score').children().remove();
+            $('#results').addClass('hidden');
+            $.each(ENGINES, function(i, engine){
+                var engine_panel = $(
+                    '<div id="panel_' + engine.id + '" class="panel panel-default">' +
+                        '<div class="panel-heading">' +
+                            '<h3 class="panel-title">'+engine.name+' <span class="label label-default pull-right"><a href="'+engine.url+'" target="_blank">View</a></span></h3>' +
+                        '</div>' +
+                        '<div class="panel-body panel-chart">' +
+                            '<canvas id="chart_' + engine.id + '" width="500" height="60">' +
+                            '<div class="progress progress-striped active"><div class="progress-bar progress-bar-success"></div></div>' +
+                        '</div>' +
+                    '</div>'
+                );
+                $('#process').append(engine_panel);
+                $('#results').removeClass('hidden');
+            });
+            var benchmark = new Benchmark(ENGINES, {'on_start': start, 'on_step': step, 'on_end': end, 'on_complete': complete});
+            benchmark.start();
+        });
+
+    });
+</script>
+
 </body>
 </html>
