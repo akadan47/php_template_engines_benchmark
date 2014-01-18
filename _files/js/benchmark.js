028b3769c9b463e8f86069a412123bc3124db6da
Denis Popov <akadan47@gmail.com>
2014-01-18 23:44:00 +0400

    Merge plain & includes diagrams.

--- _files/js/benchmark.js
+++ _files/js/benchmark.js
@@ -3,17 +3,22 @@ function Benchmark(engines, options) {
         "requests": 1000,
         "include": true,
         "on_start": function(){},
-        "on_step": function(){},
-        "on_end": function(){},
-        "on_complete": function(){}
+        "on_update": function(){},
+        "on_complete": function(){},
+        "on_finish": function(){}
     };
     $.extend(this.options, options);
 
     this.engines = engines.slice(0);
     this.request_count = this.options.requests;
+
     this.init = [];
+    this.init_includes = [];
     this.render = [];
+    this.render_includes = [];
+
     this.is_first = true;
+    this.is_include = function(){return self.request_count % 2 === 0};
 
     var self = this;
 
@@ -26,14 +31,32 @@ function Benchmark(engines, options) {
         return (sum / arr.length);
     }
 
+    function get_results (init, render) {
+        var result;
+        result = {
+            'init': {
+                'avg': (average(init) * 1000).toFixed(2),
+                'min': (Math.min.apply({}, init) * 1000).toFixed(2),
+                'max': (Math.max.apply({}, init) * 1000).toFixed(2),
+            },
+            'render': {
+                'avg': (average(render) * 1000).toFixed(2),
+                'min': (Math.min.apply({}, render) * 1000).toFixed(2),
+                'max': (Math.max.apply({}, render) * 1000).toFixed(2),
+            }
+        };
+        return result;
+    }
+
     function bench() {
         if (self.request_count > 0) {
             var params = {
                 'json': true
             };
-            if (self.is_first) {
-                params['clear'] = true
-            }
+            if (self.is_first)
+                params['clear'] = true;
+            if (self.is_include())
+                params['include'] = true;
             $.ajax({
                 type: 'GET',
                 data: params,
@@ -44,43 +67,45 @@ function Benchmark(engines, options) {
                     var time_init = json['time_init'];
                     var time_render = json['time_render'];
                     if (!self.is_first) {
-                        self.init.push(time_init);
-                        self.render.push(time_render);
-                        self.request_count -= 1;
-                        self.options.on_step(self.engines[0], (time_init*1000).toFixed(4), (time_render*1000).toFixed(4), Math.round(((self.options.requests - self.request_count)*100)/self.options.requests));
+                        if(self.is_include()){
+                            self.init_includes.push(time_init);
+                            self.render_includes.push(time_render);
+                        } else {
+                            self.init.push(time_init);
+                            self.render.push(time_render);
+                        }
                     } else {
                         self.is_first = false;
                         self.options.on_start(self.engines[0]);
                     }
+                    var percent = Math.round(((self.options.requests - self.request_count)*100)/self.options.requests).toFixed(1)
+                    self.options.on_update(
+                        self.engines[0],
+                        self.is_include(),
+                        (time_init*1000).toFixed(4),
+                        (time_render*1000).toFixed(4),
+                        percent
+                    );
+                    self.request_count -= 1;
                     bench();
                 },
                 error: bench
             });
         } else {
-            var avg_init = average(self.init) * 1000;
-            var avg_render = average(self.render) * 1000;
-            var min_init = Math.min.apply({}, self.init) * 1000;
-            var min_render = Math.min.apply({}, self.render) * 1000;
-            var max_init = Math.max.apply({}, self.init) * 1000;
-            var max_render = Math.max.apply({}, self.render) * 1000;
-            self.options.on_end(self.engines[0],
-                avg_init.toFixed(2),
-                min_init.toFixed(2),
-                max_init.toFixed(2),
-                avg_render.toFixed(2),
-                min_render.toFixed(2),
-                max_render.toFixed(2)
-            );
-
+            var results = get_results(self.init, self.render);
+            var results_includes = get_results(self.init_includes, self.render_includes);
+            self.options.on_complete(self.engines[0], results, results_includes);
             self.engines.shift();
             self.init = [];
+            self.init_includes = [];
             self.render = [];
+            self.render_includes = [];
             self.is_first = true;
             if (self.engines.length) {
                 self.request_count = self.options.requests;
                 bench();
             } else {
-                self.options.on_complete();
+                self.options.on_finish();
             }
         }
     }
--- index.php
+++ index.php
@@ -1,199 +1,197 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Typical page benchmark for PHP template engines</title>
-    <link rel="stylesheet" href="/_files/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/_files/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="/_files/css/style.css">
-    <script type="text/javascript" src="/_files/js/jquery-1.10.2.min.js"></script>
-    <script type="text/javascript" src="/_files/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="/_files/js/smoothie.js"></script>
-    <script type="text/javascript" src="/_files/js/jquery.tablesorter.min.js"></script>
-    <script type="text/javascript" src="/_files/js/benchmark.js"></script>
-    <script type="text/javascript">
-            var ENGINES = [
-                {
-                    "id": "gekkon",
-                    "name": "Gekkon",
-                    "url": "/gekkon/"
-                },
-                {
-                    "id": "smarty",
-                    "name": "Smarty",
-                    "url": "/smarty/"
-                },
-                {
-                    "id": "twig",
-                    "name": "Twig",
-                    "url": "/twig/"
-                },
-                {
-                    "id": "gekkon_includes",
-                    "name": "Gekkon (includes)",
-                    "url": "/gekkon/?include"
-                },
-                {
-                    "id": "smarty_includes",
-                    "name": "Smarty (includes)",
-                    "url": "/smarty/?include"
-                },
+    <head>
+        <meta charset="utf-8">
+        <title>Typical page benchmark for PHP template engines</title>
+        <link rel="stylesheet" href="/_files/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/_files/css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="/_files/css/style.css">
+        <script type="text/javascript" src="/_files/js/jquery-1.10.2.min.js"></script>
+        <script type="text/javascript" src="/_files/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="/_files/js/smoothie.js"></script>
+        <script type="text/javascript" src="/_files/js/jquery.tablesorter.min.js"></script>
+        <script type="text/javascript" src="/_files/js/benchmark.js"></script>
+        <script type="text/javascript">
+                var ENGINES = [
+                    {
+                        "id": "gekkon",
+                        "name": "Gekkon",
+                        "url": "/gekkon/"
+                    },
+                    {
+                        "id": "smarty",
+                        "name": "Smarty",
+                        "url": "/smarty/"
+                    },
+                    {
+                        "id": "twig",
+                        "name": "Twig",
+                        "url": "/twig/"
+                    }
+                ];
+        </script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row header">
+                <div class="col-xs-7">
+                    <h3>Typical page benchmark <br/>for PHP template engines</h3>
+                </div>
+                <div class="col-xs-5">
+                    <button class="btn btn-lg btn-warning" id="start">Start benchmark!</button>
+                </div>
+            </div>
+            <hr/>
+            <div class="row">
+                <div class="col-xs-7">
+                    <div id="process"></div>
+                </div>
+                <div id="sidebar" class="col-xs-5 hidden">
+                    <div class="bs-sidebar hidden-print affix-top">
+                        <div class="panel panel-default hidden" id="result_init">
+                            <div class="panel-heading"><h4 class="panel-title">Generation time (init & render) <span>less is the better, ms</span></h4></div>
+                            <div class="panel-body">
+                                <table class="table hidden" id="table_init">
+                                    <thead>
+                                        <tr>
+                                            <th class="name"><span>Engine</span></th>
+                                            <th><span><b>Avg</b></span></th>
+                                            <th><span>Min</span></th>
+                                            <th><span>Max</span></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody id="score_init">
 
-                {
-                    "id": "twig_includes",
-                    "name": "Twig (includes)",
-                    "url": "/twig/?include"
-                }
-            ];
-    </script>
-</head>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <div class="panel panel-default hidden" id="result_render">
+                            <div class="panel-heading"><h5 class="panel-title">Render time <span>less is the better, ms</span></h5></div>
+                            <div class="panel-body">
+                                <table class="table hidden" id="table_render">
+                                    <thead>
+                                    <tr>
+                                        <th class="name"><span>Engine</span></th>
+                                        <th><span><b>Avg</b></span></th>
+                                        <th><span>Min</span></th>
+                                        <th><span>Max</span></th>
+                                    </tr>
+                                    </thead>
+                                    <tbody id="score_render">
 
-<body>
-
-<div class="container">
-
-    <div class="row header">
-        <div class="col-xs-7">
-            <h3>Typical page benchmark <br/>for PHP template engines</h3>
-        </div>
-        <div class="col-xs-5">
-            <button class="btn btn-lg btn-warning" id="start">Start benchmark!</button>
-        </div>
-    </div>
-
-    <hr/>
-
-    <div class="row">
-
-        <div class="col-xs-7">
-            <div id="process"></div>
-        </div>
-
-        <div id="result_sidebar" class="col-xs-5 hidden">
-            <div class="bs-sidebar hidden-print affix-top">
-                <div class="panel panel-default hidden" id="result_init">
-                    <div class="panel-heading"><h4 class="panel-title">Generation time (less is the better, ms)</h4></div>
-
-                    <div class="panel-body">
-                        <table class="table hidden" id="table_init">
-                            <thead>
-                                <tr>
-                                    <th class="name">Engine</th>
-                                    <th><b>Avg</b></th>
-                                    <th>Min</th>
-                                    <th>Max</th>
-                                </tr>
-                            </thead>
-                            <tbody id="score_init">
-
-                            </tbody>
-                        </table>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
-                <div class="panel panel-default hidden" id="result_render">
-                    <div class="panel-heading"><h5 class="panel-title">Render time (less is the better, ms)</h5></div>
-
-                    <div class="panel-body">
-                        <table class="table hidden" id="table_render">
-                            <thead>
-                            <tr>
-                                <th class="name">Engine</th>
-                                <th><b>Avg</b></th>
-                                <th>Min</th>
-                                <th>Max</th>
-                            </tr>
-                            </thead>
-                            <tbody id="score_render">
-
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-
             </div>
         </div>
 
-    </div>
+        <script type="text/javascript">
+            $(function () {
+                var el_start__button = $('#start');
 
-</div>
+                var el_process = $('#process');
+                var el_sidebar = $('#sidebar');
 
+                var el_score__init = $('#score_init');
+                var el_table__init = $('#table_init');
+                var el_result__init = $('#result_init');
 
-<script type="text/javascript">
-    $(function () {
+                var el_score__render = $('#score_render');
+                var el_table__render = $('#table_render');
+                var el_result__render = $('#result_render');
 
-        function start(engine){
-            engine.line = new TimeSeries();
-            engine.chart = new SmoothieChart({millisPerPixel:50,maxValueScale:2,minValue:0,grid:{fillStyle:'#ffffff',strokeStyle:'rgba(119,119,119,0.11)',sharpLines:true,borderVisible:false},labels:{fillStyle:'#777777'}});
-            engine.chart.addTimeSeries(engine.line, {lineWidth:0.25, strokeStyle:'#03dc00', fillStyle:'#03dc00'});
-            engine.chart.streamTo(document.getElementById("chart_"+engine.id), 100);
-            engine.chart.start();
-            $('#panel_'+engine.id).addClass('panel-warning');
-        }
+                function on_start(engine){
+                    engine.line = new TimeSeries();
+                    engine.line_includes = new TimeSeries();
+                    engine.chart = new SmoothieChart({millisPerPixel:50,maxValueScale:2,minValue:0, maxValue:100,grid:{fillStyle:'#ffffff',strokeStyle:'rgba(119,119,119,0.11)',sharpLines:true,borderVisible:false},labels:{fillStyle:'#777777'}});
+                    engine.chart.addTimeSeries(engine.line_includes, {lineWidth:0.25, strokeStyle:'#00acff', fillStyle:'#00acff'});
 
-        function step(engine, time_init, time_render, count) {
-            $('#bar_'+engine.id).css('width', count+"%");
-            engine.line.append(new Date().getTime(), time_init);
-        }
+                    engine.chart.addTimeSeries(engine.line, {lineWidth:0.25, strokeStyle:'#58fa46', fillStyle:'#58fa46'});
 
-        function end(engine, avg_init, min_init, max_init, avg_render, min_render, max_render) {
-            engine.chart.stop();
-            if ($('#score_init').children()) {
-                $('#table_init').removeClass('hidden');
+                    engine.chart.streamTo(document.getElementById("chart_"+engine.id), 100);
+                    engine.chart.start();
+                    $('#panel_'+engine.id).addClass('panel-warning');
+                }
 
-            }
-            if ($('#score_render').children()) {
-                $('#table_render').removeClass('hidden');
-            }
-            $('#bar_'+engine.id).css('opacity', '0.15');
-            $('.panel-warning').removeClass('panel-warning');
-            $('#score_init').append($('<tr><td class="name"><b>'+engine.name+'</b></td><td><b>'+avg_init+'</b></td><td>'+min_init+'</td><td>'+max_init+'</td></tr>'));
-            $('#score_render').append($('<tr><td class="name"><b>'+engine.name+'</b></td><td><b>'+avg_render+'</b></td><td>'+min_render+'</td><td>'+max_render+'</td></tr>'));
-        }
+                function on_update(engine, is_include, time_init, time_render, percent) {
+                    $('#bar_'+engine.id).css('width', percent+"%");
+                    if (is_include) {
+                        engine.line_includes.append(new Date().getTime(), time_init);
+                    } else {
+                        engine.line.append(new Date().getTime(), time_init);
+                    }
 
-        function complete() {
-            $("#table_init").trigger('update').tablesorter();
-            $("#table_render").trigger('update').tablesorter();
-            $('#result_init').addClass('panel-warning');
-            $('#result_render').addClass('panel-warning');
-            $('#start').text('Restart Benchmark!').removeAttr('disabled');
-        }
+                }
 
-        $('#start').on('click', function(){
-            $(this).attr('disabled', 'disabled');
-            $('#process').children().remove();
-            $('#score_init').children().remove();
-            $('#score_render').children().remove();
-            $('#table_init').addClass('hidden');
-            $('#table_render').addClass('hidden');
-            $('#result_init').addClass('hidden');
-            $('#result_render').addClass('hidden');
-            $.each(ENGINES, function(i, engine){
-                var engine_panel = $(
-                    '<div id="panel_' + engine.id + '" class="panel panel-default">' +
-                        '<div class="panel-heading">' +
-                            '<h3 class="panel-title">'+engine.name+' <span class="label label-default pull-right"><a href="'+engine.url+'" target="_blank">HTML</a></span></h3>' +
-                        '</div>' +
-                        '<div class="panel-body panel-chart">' +
-                            '<canvas id="chart_' + engine.id + '" width="524" height="77"></canvas>' +
-                            '<div class="progress"><div id="bar_' + engine.id + '" class="progress-bar progress-bar-primary"></div></div>' +
-                        '</div>' +
-                    '</div>'
-                );
-                $('#process').append(engine_panel);
-                $('#result_init').removeClass('hidden').removeClass('panel-warning');
-                $('#result_render').removeClass('hidden').removeClass('panel-warning');
-                $('#result_sidebar').removeClass('hidden');
+                function on_complete(engine, results, results_includes) {
+                    engine.chart.stop();
+                    if (el_score__init.children())
+                        el_table__init.removeClass('hidden');
+                    if (el_score__render.children())
+                        el_table__render.removeClass('hidden');
+                    $('#bar_'+engine.id).css('opacity', '0.15');
+                    $('.panel-warning').removeClass('panel-warning');
+                    el_score__init.append($('<tr><td class="name"><b>'+engine.name+'</b></td><td><b>'+results.init.avg+'</b></td><td>'+results.init.min+'</td><td>'+results.init.max+'</td></tr>'));
+                    el_score__init.append($('<tr><td class="name"><b>'+engine.name+' with includes</b></td><td><b>'+results_includes.init.avg+'</b></td><td>'+results_includes.init.min+'</td><td>'+results_includes.init.max+'</td></tr>'));
 
+                    el_score__render.append($('<tr><td class="name"><b>'+engine.name+'</b></td><td><b>'+results.render.avg+'</b></td><td>'+results.render.min+'</td><td>'+results.render.max+'</td></tr>'));
+                    el_score__render.append($('<tr><td class="name"><b>'+engine.name+' with includes</b></td><td><b>'+results_includes.render.avg+'</b></td><td>'+results_includes.render.min+'</td><td>'+results_includes.render.max+'</td></tr>'));
+
+                    el_table__init.trigger("update");
+                    el_table__render.trigger("update");
+                }
+
+                function on_finish() {
+                    var sorting = [[1,0]];
+
+                    el_table__init.tablesorter().trigger("sorton",[sorting]);
+                    el_result__init.addClass('panel-warning');
+
+                    el_table__render.tablesorter().trigger("sorton",[sorting]);
+                    el_result__render.addClass('panel-warning');
+                    el_start__button.text('Restart Benchmark!').removeAttr('disabled');
+                }
+
+                el_start__button.on('click', function(){
+                    el_start__button.attr('disabled', 'disabled');
+                    el_process.children().remove();
+                    el_score__init.children().remove();
+                    el_table__init.addClass('hidden');
+                    el_result__init.addClass('hidden');
+                    el_score__render.children().remove();
+                    el_table__render.addClass('hidden');
+                    el_result__render.addClass('hidden');
+
+                    $.each(ENGINES, function(i, engine){
+                        var engine_panel = $(
+                            '<div id="panel_' + engine.id + '" class="panel panel-default">' +
+                                '<div class="panel-heading">' +
+                                    '<h3 class="panel-title">'+engine.name+' <span class="pull-right"><span class="point point-plain"></span> <a href="'+engine.url+'" target="_blank">Plain</a> <span class="point point-includes"></span> <a href="'+engine.url+'?include" target="_blank">Includes</a></span> </h3>' +
+                                '</div>' +
+                                '<div class="panel-body panel-chart">' +
+                                    '<canvas id="chart_' + engine.id + '" width="524" height="77"></canvas>' +
+                                    '<div class="progress active  progress-striped"><div id="bar_' + engine.id + '" class="progress-bar progress-bar-warning"></div></div>' +
+                                '</div>' +
+                            '</div>'
+                        );
+                        el_process.append(engine_panel);
+                        el_result__init.removeClass('hidden').removeClass('panel-warning');
+                        el_result__render.removeClass('hidden').removeClass('panel-warning');
+                        el_sidebar.removeClass('hidden');
+                    });
+                    var benchmark = new Benchmark(ENGINES, {
+                        'on_start': on_start,
+                        'on_update': on_update,
+                        'on_complete': on_complete,
+                        'on_finish': on_finish
+                    });
+                    benchmark.start();
+                });
             });
-            var benchmark = new Benchmark(ENGINES, {'on_start': start, 'on_step': step, 'on_end': end, 'on_complete': complete});
-            benchmark.start();
-        });
-
-    });
-</script>
-
-</body>
+        </script>
+    </body>
 </html>
