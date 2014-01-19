f8cd3bb03f73e219e1e312a1fbf5f41c81b84833
Denis Popov <akadan47@gmail.com>
2014-01-19 10:39:47 +0400

    Fix nowarp tag for gekkon 3.15

--- engines/gekkon_3.15/plugin/nowarp.php
+++ engines/gekkon_3.15/plugin/nowarp.php
@@ -1,3 +1,6 @@
 <?php
-$tag['inner']=preg_replace('/>\s+</Uis','><', preg_replace('/\s+/is',' ', $tag['inner']));
+$bin_open="<?php ob_start(); ?>";
+$bin_close="<?php \$content = ob_get_contents();".
+           "ob_end_clean();\n".
+           "echo preg_replace('/>\s+</Uis', '><',preg_replace('/\s+/is', ' ', \$content));\n";
 ?>
