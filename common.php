7c3cca8b23e66e542005065143d2a824795707a3
Denis Popov <akadan47@gmail.com>
2014-01-23 09:24:01 +0400

    remove time

--- common.php
+++ common.php
@@ -7,7 +7,7 @@ function print_time($is_json, $generation, $init, $render)
 {
     if ($is_json) {
         header('Content-type: application/json');
-        echo json_encode(array("time"=>microtime(), "generation"=>$generation, "init"=>$init, "render"=>$render));
+        echo json_encode(array("generation"=>$generation, "init"=>$init, "render"=>$render));
     } else
         echo "<div id=\"time\"><b>Time</b>: ".$generation." <b>Init</b>: ".$init." <b>Render</b>: ".$render."</div>";
 }
